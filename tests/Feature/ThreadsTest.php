7280add865cde6a45a11104d05f2da7f1ece8a82
Fred Zhou <fredpaw@gmail.com>
2017-10-11 22:08:13 +1100

    ver 0.02

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,19 +10,22 @@ class ThreadsTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
         $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads');
-
         $response->assertSee($thread->title);
-
-        $response = $this->get('/threads/', $thread->id);
-
-        $response->assertSee($thread->title);
-
-
     }
+
+    /** @test **/
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/', ['thread' => $thread->id]);
+        $response->assertSee($thread->title);
+    }
+
 
 }
