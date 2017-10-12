1da515b7197ccc785e3785ed049720af7233af2b
Fred <fredpaw@gmail.com>
2017-10-12 16:11:44 +1100

    ver 0.03

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,11 @@ class Thread extends Model
 {
     public function path()
     {
-        return '/threads/'.$this->id;
+        return 'threads/'.$this->id;
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
