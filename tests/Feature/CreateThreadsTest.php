f8bd781230c01a388f5e35e8ce3a0f3d6734e508
Fred <fredpaw@gmail.com>
2017-10-24 17:07:58 +1100

    ver 0.06

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post(url('/').'/threads', $thread->toArray());
+    }
+
+    /** @test **/
+    public function an_authenticated_user_can_create_new_form_thread()
+    {
+        // Given we have a signed in user
+        $this->actingAs(factory('App\User')->create());
+
+        // When we hit the endpoint ot create a new thread
+        $thread = factory('App\Thread')->make();
+
+        $this->post(url('/').'/threads', $thread->toArray());
+
+        // Then, when we visit the thread page. We should see the new thread
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
