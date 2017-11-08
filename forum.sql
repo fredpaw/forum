ed4bb7085be167e1adcfc4ae6260fd56a66cff13
Fred <fredpaw@gmail.com>
2017-11-08 16:22:13 +1100

    Ver 0.11

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-10-27 09:18:48
+-- Generation Time: 2017-11-08 06:20:04
 -- 服务器版本： 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -29,13 +29,46 @@ class CreateThreadsTest extends TestCase
         $this->signIn();
 
         // When we hit the endpoint ot create a new thread
-        $thread = create('App\Thread');
+        $thread = make('App\Thread');
 
-        $this->post(url('/').'/threads', $thread->toArray());
+        $response = $this->post(url('/').'/threads', $thread->toArray());
 
         // Then, when we visit the thread page. We should see the new thread
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
+    }
+
+    /** @test **/
+    public function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    public function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 }
