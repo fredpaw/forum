ca916dbd5c3b78557900f8b033822e0408f7fd76
Fred <fredpaw@gmail.com>
2017-11-27 14:18:09 +1100

    ver 0.18

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Reply;
+use App\Favorite;
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite(auth()->id());
+//        $reply->favorites()->create(['user_id' => auth()->id()]);
+    }
+}
