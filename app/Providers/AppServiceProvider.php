e7ca080f5efaeee0580019d71699a3e776af10ac
Fred Zhou <fredpaw@gmail.com>
2018-08-29 13:31:19 +1000

    small change on appserverce

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
             });
             $view->with('channels', $channels);
         });
+//        \View::share('channels', Channel::all());
     }
 
     /**
